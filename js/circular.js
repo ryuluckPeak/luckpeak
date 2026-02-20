b7199369a3b858ed4ff80457aade68a3dde3bfc4
ryu yoshimine <141909055+ryuluckPeak@users.noreply.github.com>
2026-02-20 12:34:00 +0900

    Add files via upload

--- js/circular.js
+++ js/circular.js
@@ -1,39 +1,19 @@
-function positionMenu() {
+// Demo by http://creative-punch.net 
+  var items = document.querySelectorAll('.circle a'); 
 
-  const circle = document.querySelector('.circle');
-  const items = document.querySelectorAll('.circle a');
+   for(var i = 0, l = items.length; i < l; i++) 
 
-  const circleSize = circle.offsetWidth;
+ { 
+    items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*
+     (1/l)*i*Math.PI)).toFixed(4) + "%"; 
 
-  // 半径を自動計算
-  const radius = circleSize / 2.5;
+    items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*
+    (1/l)*i*Math.PI)).toFixed(4) + "%"; 
+  } 
 
-  const length = items.length;
-
-  items.forEach((item, i) => {
-
-    const angle = -0.5 * Math.PI + (2 * Math.PI * i / length);
-
-    item.style.left =
-      (50 + radius * Math.cos(angle) / circleSize * 100) + "%";
-
-    item.style.top =
-      (50 + radius * Math.sin(angle) / circleSize * 100) + "%";
-  });
-}
-
-/* 初期配置 */
-positionMenu();
-
-/* 画面サイズ変わったら再計算（超重要） */
-window.addEventListener("resize", positionMenu);
-
-
-/* 開閉ボタン */
-document.querySelector('.menu-button').onclick = function(e){
-  e.preventDefault();
-  document.querySelector('.circle').classList.toggle('open');
-};
-
-
+document.querySelector('.menu-button').onclick = function(e)
+ {
+    e.preventDefault();
+    document.querySelector('.circle').classList.toggle('open');
+ }
 
